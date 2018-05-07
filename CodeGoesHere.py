331b9f1859465e7373c5c58dcb01c02174313baf
Llewellyn Falco <isidore@setgame.com>
2018-05-07 15:45:37 +0200

    Sample Tests

---
+++ CodeGoesHere.py
@@ -0,0 +1,2 @@
+def sample():
+    return "hello world"
---
+++ tests/SampleTest.py
@@ -0,0 +1,23 @@
+import unittest
+
+from approvaltests.approvals import verify
+
+
+from approvaltests.reporters.generic_diff_reporter_factory import GenericDiffReporterFactory
+
+
+
+class RegressionTest(unittest.TestCase):
+    def setUp(self):
+        self.reporter = GenericDiffReporterFactory().get("DiffMerge")
+        #Download DiffMerge at https://sourcegear.com/diffmerge/
+
+    def test_straight_unittest(self):
+        self.assertEqual(5,5)
+
+
+    def test_with_approvals(self):
+        from CodeGoesHere import sample
+        verify(sample(), self.reporter)
+
+
