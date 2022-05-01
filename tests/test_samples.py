e4d4221676be189aa20e283009f5950f1918de6b
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-05-01 01:57:54 +0000

    t redid tests to make it easier for beginners

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -5,30 +5,39 @@ import pytest
 from approvaltests.approvals import verify, verify_all
 
 from approvaltests.reporters.python_native_reporter import PythonNativeReporter
-from approvaltests import Options
+from approvaltests import Options, verify_as_json
 from approvaltests.reporters.generic_diff_reporter_factory import GenericDiffReporterFactory
 from approvaltests import set_default_reporter
 
-def test_pytest():
-    assert True
 
 class SampleTests(unittest.TestCase):
     def setUp(self):
-        set_default_reporter(None) #Use the first difftool found on your system
-        #self.reporter = GenericDiffReporterFactory().get("DiffMerge")
-        #Download DiffMerge at https://sourcegear.com/diffmerge/
+        set_default_reporter(None)  # Use the first difftool found on your system
+        # self.reporter = GenericDiffReporterFactory().get("DiffMerge")
+        # Download DiffMerge at https://sourcegear.com/diffmerge/
 
     def test_straight_unittest(self):
-        self.assertEqual(5,5)
+        self.assertEqual(5, 5)
 
     def test_with_approvals(self):
+        sample = "Welcome To Approvals"
+        verify(sample)
+
+    def test_with_approvals_from_project_code(self):
         from project.sample_function import this_is_the_function
         verify(this_is_the_function())
 
+    def test_with_json(self):
+        sample = {"firstName": "jayne",
+                  "lastName": "cobb",
+                  "isMale": True,
+                  "age": 38}
+        verify_as_json(sample)
+
     def test_list_with_reporter(self):
         sample = ["welcome", "to", "approvals"]
-        verify_all("words", sample,options=Options().with_reporter(PythonNativeReporter()))        
-
-            
+        verify_all("words", sample, options=Options().with_reporter(PythonNativeReporter()))
 
 
+def test_pytest():
+    assert True
